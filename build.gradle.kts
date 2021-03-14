8c695d61b43fccc6d0dad1a7d1ce2ca894ca72ba
David Wursteisen <david.wursteisen+git@gmail.com>
2021-03-14 16:05:09 +0100

    Prepare for deployement in sonatype repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,24 @@ dependencies {
     testImplementation(kotlin("test-junit"))
     testImplementation("junit:junit:4.12")
 }
+
+minigdxDeveloper {
+    this.name.set("gltf-loader")
+    this.description.set("Loader for glTF2.0 files written in kotlin.")
+    this.projectUrl.set("https://github.com/minigdx/gltf-loader")
+    this.licence {
+        name.set("MIT Licence")
+        url.set("https://github.com/minigdx/gltf-loader/blob/develop/LICENSE")
+    }
+    developer {
+        name.set("David Wursteisen")
+        email.set("david.wursteisen+minigdx@gmail.com")
+        url.set("https://github.com/dwursteisen")
+    }
+
+    developer {
+        name.set("Adrien Bennadji")
+        email.set("adrien.bennadji@live.fr")
+        url.set("https://github.com/adrien-ben")
+    }
+}
