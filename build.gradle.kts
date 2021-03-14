47ede4aa34c98ad79b6c0eca9cf974f5ad18c41d
David Wursteisen <david.wursteisen+git@gmail.com>
2021-03-14 08:31:46 +0100

    Prepare for deployement

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.github.minigdx.gradle.plugin.developer.jvm") version "1.0.0-alpha1"
+    id("com.github.minigdx.gradle.plugin.developer.jvm") version "1.0.0-alpha0"
     id("jacoco")
 }
 
